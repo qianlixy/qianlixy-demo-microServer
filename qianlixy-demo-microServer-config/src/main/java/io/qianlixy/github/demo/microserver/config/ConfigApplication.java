a909801e258f9513bf663ca93598940977ae94ee
Qianli <qianli_xy@163.com>
2018-12-04 12:11:24 +0800

    本地配置文件调整

--- qianlixy-demo-microServer-config/src/main/java/io/qianlixy/github/demo/microserver/config/ConfigApplication.java
+++ qianlixy-demo-microServer-config/src/main/java/io/qianlixy/github/demo/microserver/config/ConfigApplication.java
@@ -1,11 +1,13 @@
 package io.qianlixy.github.demo.microserver.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableConfigServer
+@EnableEurekaClient
 public class ConfigApplication {
 
 	public static void main(String[] args) {
